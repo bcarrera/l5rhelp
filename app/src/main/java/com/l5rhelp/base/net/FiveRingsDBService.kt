d9c56df3e8ec7086ffc5650d176a3f8c0a7672d4
bcarrera <modulandia@hotmail.com>
2018-02-18 19:54:24 +0100

    retrofit configuration

---
+++ app/src/main/java/com/l5rhelp/base/net/FiveRingsDBService.kt
@@ -0,0 +1,27 @@
+package com.l5rhelp.base.net
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface FiveRingsDBService {
+
+    //Cards
+    @GET("/cards")
+    fun getAllCards(): Call<String>
+
+    @GET("/cards/{code}")
+    fun getCardByCode(@Path(value="code") code: String): Call<String>
+
+    //Rulings
+    @GET("/rulings")
+    fun getAllRulings(): Call<String>
+
+    @GET("/cards/{card_id}/rulings")
+    fun getRulingByCardId(@Path(value="card_id") card_id: String): Call<String>
+
+    @GET("/cards/{card_id}/rulings/{ruling_id}")
+    fun getRulingDetail(@Path(value="card_id") card_id: String, @Path(value="ruling_id") ruling_id: String): Call<String>
+
+}

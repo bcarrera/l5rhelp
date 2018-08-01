a41421f6d9ff1a673c269a4987daaac35219cc43
bcarrera <modulandia@hotmail.com>
2018-08-01 20:08:30 +0200

    new design for filters dialog

--- app/src/main/java/com/l5rhelp/ui/utils/FiltersDialog.kt
+++ app/src/main/java/com/l5rhelp/ui/utils/FiltersDialog.kt
@@ -5,6 +5,10 @@ import android.content.Context
 import android.os.Bundle
 import com.l5rhelp.R
 import kotlinx.android.synthetic.main.filters_dialog.*
+import android.view.Gravity
+import android.view.WindowManager
+
+
 
 
 class FiltersDialog(context: Context?, val type : CardsSearchFilters) : Dialog(context) {
@@ -15,22 +19,73 @@ class FiltersDialog(context: Context?, val type : CardsSearchFilters) : Dialog(c
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.filters_dialog)
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT)
+        window.setGravity(Gravity.CENTER)
 
         when(type){
             CardsSearchFilters.CLAN -> {
                 cards_filters_type_checkboxes.hide()
                 cards_filters_deck_checkboxes.hide()
                 cards_filters_clans_checkboxes.show()
+                cards_filters_select_all_checkbox.setOnClickListener{
+                    if (!cards_filters_select_all_checkbox.isChecked) {
+                        clan_crab_checkbox.isChecked = false
+                        clan_crane_checkbox.isChecked = false
+                        clan_dragon_checkbox.isChecked = false
+                        clan_lion_checkbob.isChecked = false
+                        clan_neutral_checkbox.isChecked = false
+                        clan_phoenix_checkbox.isChecked = false
+                        clan_scorpion_checkbox.isChecked = false
+                        clan_unicorn_checkbox.isChecked = false
+                    } else {
+                        clan_crab_checkbox.isChecked  = true
+                        clan_crane_checkbox.isChecked  = true
+                        clan_dragon_checkbox.isChecked  = true
+                        clan_lion_checkbob.isChecked  = true
+                        clan_neutral_checkbox.isChecked  = true
+                        clan_phoenix_checkbox.isChecked  = true
+                        clan_scorpion_checkbox.isChecked  = true
+                        clan_unicorn_checkbox.isChecked  = true
+                    }
+                }
             }
             CardsSearchFilters.TYPE -> {
                 cards_filters_deck_checkboxes.hide()
                 cards_filters_clans_checkboxes.hide()
                 cards_filters_type_checkboxes.show()
+                cards_filters_select_all_checkbox.setOnClickListener{
+                    if (!cards_filters_select_all_checkbox.isChecked) {
+                        type_attachment_checkbox.isChecked = false
+                        type_character_checkbox.isChecked = false
+                        type_event_checkbox.isChecked = false
+                        type_holding_checkbox.isChecked = false
+                        type_province_checkbox.isChecked = false
+                        type_role_checkbox.isChecked = false
+                        type_stronghold_checkbox.isChecked = false
+                    } else {
+                        type_attachment_checkbox.isChecked  = true
+                        type_character_checkbox.isChecked  = true
+                        type_event_checkbox.isChecked  = true
+                        type_holding_checkbox.isChecked  = true
+                        type_province_checkbox.isChecked  = true
+                        type_role_checkbox.isChecked  = true
+                        type_stronghold_checkbox.isChecked  = true
+                    }
+                }
             }
             CardsSearchFilters.DECK -> {
                 cards_filters_type_checkboxes.hide()
                 cards_filters_clans_checkboxes.hide()
                 cards_filters_deck_checkboxes.show()
+                cards_filters_select_all_checkbox.setOnClickListener{
+                    if (!cards_filters_select_all_checkbox.isChecked) {
+                        deck_conflict_checkbox.isChecked = false
+                        deck_dinasty_checkbox.isChecked = false
+                    } else {
+                        deck_conflict_checkbox.isChecked  = true
+                        deck_dinasty_checkbox.isChecked  = true
+                    }
+                }
             }
             CardsSearchFilters.PACK -> TODO()
         }
